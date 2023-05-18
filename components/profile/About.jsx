8256a60dd1394b0bf47db84eda70c8246e1ebf0c
¨sktbrd¨ <sktbrd.eth@gmail.com>
2023-05-18 01:23:46 -0300

    user about

--- components/profile/About.jsx
+++ components/profile/About.jsx
@@ -1,14 +1,54 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import useAuthUser from '../../pages/api/UseAuthUser';
-import { Client } from "@hiveio/dhive";
+import { Client } from '@hiveio/dhive';
+import styles from '../../styles/About.module.css';
 
-const About = () => {
+const client = new Client([
+  'https://api.hive.blog',
+  'https://anyx.io',
+  'https://api.pharesim.me'
+]);
+
+const ProfileItem = ({ label, value }) => (
+  <tr>
+    <td>{label}:</td>
+    <td>{value}</td>
+  </tr>
+);
+
+export default function About() {
+  const { user } = useAuthUser();
+  const [isLoading, setIsLoading] = useState(true);
+  const [profileAbout, setProfileAbout] = useState('');
+
+  useEffect(() => {
+    if (user) {
+      setIsLoading(false);
+      const metadata = JSON.parse(user.posting_json_metadata);
+      if (metadata && metadata.profile && metadata.profile.about) {
+        setProfileAbout(metadata.profile.about);
+      }
+    }
+  }, [user]);
+
   return (
-    <div>
-      <h1>Hello, World!</h1>
-      <iframe src="https://discord.com/widget?id=631777256234156033&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
+    <div className={styles.container}>
+      {isLoading ? (
+        <div className={styles.loading}>Loading...</div>
+      ) : (
+        <div>
+          <h1 className={styles.heading}>User Account Resume</h1>
+          <h1>{profileAbout}</h1>
+
+          <table className={styles.table}>
+            <tbody>
+              <ProfileItem label="ID" value={user.id} />
+              <ProfileItem label="Name" value={user.name} />
+              <ProfileItem label="About" value={profileAbout} />
+            </tbody>
+          </table>
+        </div>
+      )}
     </div>
   );
-};
-
-export default About;
+}
