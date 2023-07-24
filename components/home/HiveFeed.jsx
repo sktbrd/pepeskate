e2a0f61cded62009f9d998a14363ad635ed0c887
¨sktbrd¨ <sktbrd.eth@gmail.com>
2023-07-24 16:55:23 -0300

    postEarning in postCard

--- components/home/HiveFeed.jsx
+++ components/home/HiveFeed.jsx
@@ -2,25 +2,48 @@ import { useEffect, useState } from "react";
 import { Client } from "@hiveio/dhive";
 import styles from "./HiveFeed.module.css";
 import PostModal from "../post/PostModal";
-import LoadingBar from '../fx/LoadingBar.jsx';
+import LoadingBar from "../fx/LoadingBar.jsx";
 
-const client = new Client([
+const nodes = [
+  "https://rpc.ecency.com",
+  "https://api.deathwing.me",
+  "https://api.hive.blog",
+  "https://api.openhive.network",
   "https://api.hive.blog",
   "https://anyx.io",
   "https://api.pharesim.me",
-]);
+];
 
-// Function to calculate post earnings (placeholder value: $69.42)
-const postEarnings = () => {
-  const placeholderEarnings = 69.42; // Replace with actual earnings data from the post
-  return `$${placeholderEarnings.toFixed(2)}`;
-};
+const defaultThumbnail = "https://images.ecency.com/u/hive-173115/avatar/large";
+const placeholderEarnings = 69.42; // Replace with actual placeholder value
 
 export default function HiveBlog(query) {
   const [posts, setPosts] = useState([]);
   const [tag, setTag] = useState("skatehive"); // set the default search author to "skatehive"
   const [isLoading, setIsLoading] = useState(false);
   const [selectedPost, setSelectedPost] = useState(null);
+  const [client, setClient] = useState(new Client(nodes[0]));
+  const [nodeIndex, setNodeIndex] = useState(0);
+
+  const fetchPostEarnings = async (author, permlink) => {
+    try {
+      const post = await client.database.call("get_content", [author, permlink]);
+      const totalPayout = parseFloat(post.total_payout_value.split(" ")[0]);
+      const curatorPayout = parseFloat(post.curator_payout_value.split(" ")[0]);
+      const pendingPayout = parseFloat(post.pending_payout_value.split(" ")[0]);
+      const totalEarnings = totalPayout + curatorPayout + pendingPayout;
+      return totalEarnings;
+    } catch (error) {
+      // If a request fails, switch to the next node
+      const newIndex = (nodeIndex + 1) % nodes.length;
+      setNodeIndex(newIndex);
+      setClient(new Client(nodes[newIndex]));
+      console.log(`Switched to node: ${nodes[newIndex]}`);
+      // Retry the request with the new node
+      return fetchPostEarnings(author, permlink);
+    }
+  };
+  
 
   const fetchPosts = async () => {
     setIsLoading(true);
@@ -34,9 +57,26 @@ export default function HiveBlog(query) {
 
       const postsWithThumbnails = result.map((post) => {
         const metadata = JSON.parse(post.json_metadata);
-        const thumbnail = metadata?.image?.[0];
-        return { ...post, thumbnail };
+        const thumbnail =
+          Array.isArray(metadata?.image) && metadata.image.length > 0
+            ? metadata.image[0]
+            : defaultThumbnail;
+        return { ...post, thumbnail, earnings: 0 }; // Initialize earnings to 0
       });
+
+      // Fetch earnings for each post concurrently
+      const earningsPromises = postsWithThumbnails.map((post) =>
+        fetchPostEarnings(post.author, post.permlink).catch((error) => {
+          console.log(error);
+          return placeholderEarnings; // Use placeholder value if fetching actual earnings fails
+        })
+      );
+      const earnings = await Promise.all(earningsPromises);
+
+      // Update earnings for each post
+      for (let i = 0; i < postsWithThumbnails.length; i++) {
+        postsWithThumbnails[i].earnings = earnings[i];
+      }
 
       setPosts(postsWithThumbnails);
     } catch (error) {
@@ -52,7 +92,7 @@ export default function HiveBlog(query) {
 
   const handlePostClick = (post) => {
     setSelectedPost(post);
-    console.log(post.body)
+    console.log(post.body);
   };
 
   const handleModalClose = () => {
@@ -73,7 +113,14 @@ export default function HiveBlog(query) {
         </div>
       )}
       {isLoading ? (
-        <center style={{ position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)" }}>
+        <center
+          style={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+          }}
+        >
           <LoadingBar />
         </center>
       ) : (
@@ -94,12 +141,18 @@ export default function HiveBlog(query) {
                   <p className={styles.authorName}>{post.author}</p>
                 </div>
                 <br></br>
-                <img src={post.thumbnail} alt="post thumbnail" className={styles.postThumbnail} />
+                <img
+                  src={post.thumbnail}
+                  alt="post thumbnail"
+                  className={styles.postThumbnail}
+                />
                 <br></br>
                 <p className={styles.postTitle}>{post.title}</p>
 
                 {/* Display post earnings */}
-                <div className={styles.postEarnings}>{postEarnings()}</div>
+                <div className={styles.postEarnings}>
+                  {`$${post.earnings.toFixed(2)}`}
+                </div>
               </div>
             </a>
           ))}
